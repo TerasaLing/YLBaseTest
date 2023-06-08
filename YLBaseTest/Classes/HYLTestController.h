7e888ba0bbb063c481ec9a7869ef28b9b5cb45af
TerasaLing <1287835607@qq.com>
2023-06-08 16:42:53 +0800

    添加测试代码

---
+++ YLBaseTest/Classes/HYLTestController.h
@@ -0,0 +1,17 @@
+//
+//  HYLTestController.h
+//  YLBaseTest_Example
+//
+//  Created by admin on 6/8/23.
+//  Copyright © 2023 TerasaLing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HYLTestController : NSObject
+-(void)testPrint;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YLBaseTest/Classes/HYLTestController.m
@@ -0,0 +1,15 @@
+//
+//  HYLTestController.m
+//  YLBaseTest_Example
+//
+//  Created by admin on 6/8/23.
+//  Copyright © 2023 TerasaLing. All rights reserved.
+//
+
+#import "HYLTestController.h"
+
+@implementation HYLTestController
+-(void)testPrint{
+    NSLog(@"我的第一个pod项目打印测试");
+}
+@end
